8c76a480440402f51a92be112766d1d224f4c7b9
Milo <JosephMRDominguez@gmail.com>
2024-08-12 19:05:12 -0700

    Added Dict Creation/Deletion

--- dictionary/client/src/pages/create.js
+++ dictionary/client/src/pages/create.js
@@ -1,18 +1,48 @@
 import React from 'react';
-
-import '../styles/create.css'
+import {useState} from 'react';
+import '../styles/global.css';
+import '../styles/create.css';
 import CreateDatabaseForm from '../components/createDatabaseForm';
+import ListDatabasesMenu from '../components/listDatabases';
+import { deleteDatabase } from '../components/backRequests';
 
 function Create(){
+    const [selectedDb, setSelectedDb] = useState("");
+
+
+    const handleDeleteSubmit = async (e) => {
+        e.preventDefault();
+        console.log(`${selectedDb}`);
+        if(selectedDb){
+            try{
+                const response = await deleteDatabase(selectedDb);
+                console.log('Database deleted:', response.data);
+                window.location.reload(); 
+            }
+            catch (error){
+                console.error('Error deleting database:', error.message);
+            }
+        }
+        else{
+            console.log('No database selected');
+        }
+    }
+
     return(
         <div className="container">
             <div className="header">
-                <h1>Header</h1>
+                <h1>Buttons to go to home, add, review go here</h1>
             </div>
 
             <div className="content">
-                <h1>Create a New Database</h1>
+                <h1>Create a New Dictionary</h1>
                 <CreateDatabaseForm />
+                <br></br>
+                <h2>Delete a Dictionary</h2>
+                <form onSubmit={handleDeleteSubmit}>
+                    <ListDatabasesMenu onSelectDb={setSelectedDb}/>
+                    <button type="submit">Delete Dictionary</button>
+                </form>
             </div>
 
         </div>
